1ca2191cc60f578789ffb5be877f520c06f54cb7
julin.serg <julin.serg@gmail.com>
2022-11-29 15:58:21 +0300

    fix lint

--- internal/server/http/middleware.go
+++ internal/server/http/middleware.go
@@ -1,11 +1,1 @@
 package internalhttp
-
-import (
-	"net/http"
-)
-
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO
-	})
-}
