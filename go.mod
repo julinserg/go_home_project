50c2c7344cc0e8998dd26c2ccce600c6e94cac7c
julin.serg <julin.serg@gmail.com>
2022-12-08 19:20:33 +0300

    crop and resize image

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v1.2.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/oliamb/cutter v0.2.2
 	github.com/rs/zerolog v1.28.0
 	github.com/stretchr/testify v1.8.1
 )
--- internal/server/http/handlers.go
+++ internal/server/http/handlers.go
@@ -1,12 +1,20 @@
 package internalhttp
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"errors"
+	"image/jpeg"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/nfnt/resize"
+	"github.com/oliamb/cutter"
 )
 
 var (
@@ -72,9 +80,52 @@ func (ph *previewerHandler) mainHandler(w http.ResponseWriter, r *http.Request) 
 	}
 	defer res.Body.Close()
 
-	_ = width
-	_ = height
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		ph.sendError(err, http.StatusInternalServerError, w)
+		return
+	}
 
-	io.Copy(w, res.Body)
+	f, err := os.Create("./pic.jpg")
+	if err != nil {
+		ph.sendError(err, http.StatusInternalServerError, w)
+		return
+	}
+	wf := bufio.NewWriter(f)
+	wf.Write(bodyBytes)
+
+	// decode jpeg into image.Image
+	readerJpeg := bytes.NewReader(bodyBytes)
+	img, err := jpeg.Decode(readerJpeg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srcX := img.Bounds().Dx()
+	srcY := img.Bounds().Dy()
+	srcP := srcX / srcY
+	dstP := width / height
+	if srcP != dstP {
+		dstX := srcY * width / height
+		dstY := srcX * height / width
+		img, err = cutter.Crop(img, cutter.Config{
+			Width:  dstX,
+			Height: dstY,
+			Mode:   cutter.Centered,
+		})
+	}
+	resizeImage := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
+
+	out, err := os.Create("./test_resized.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// write new image to file
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, resizeImage, nil)
+	out.Write(buf.Bytes())
+	w.Write(buf.Bytes())
 	w.WriteHeader(res.StatusCode)
 }
